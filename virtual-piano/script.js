06ade3d9448e75000468674f95b70f29ee9f2ea8
Vitebsk121 <Alexandr.demchenko@list.ru>
2021-03-21 13:13:57 +0300

    feat:  add note-letter btn code, fullscreen btn code.

--- virtual-piano/script.js
+++ virtual-piano/script.js
@@ -1,12 +1,41 @@
 'use strict';
+const pianoKeys = document.querySelectorAll('.piano-key');
+const PIANO = document.getElementById('piano');
+const Fullscreen = document.querySelector('.fullscreen');
+const btnNotes = document.querySelector('.btn-notes');
+const btnLetters = document.querySelector('.btn-letters');
+
+const notesActive = () => {
+    btnNotes.classList.add('btn-active');
+    btnLetters.classList.remove('btn-active');
+    pianoKeys.forEach((elem) => elem.classList.remove('piano-key-letter'));
+};
+
+const lettersActive = () => {
+    btnLetters.classList.add('btn-active');
+    btnNotes.classList.remove('btn-active');
+    pianoKeys.forEach((elem) => elem.classList.add('piano-key-letter'));
+};
+
+const startStopFullscreen = () => {
+    let elem = document.querySelector('body')
+    if (!document.fullscreenElement) {
+        elem.requestFullscreen()
+    } else {
+        document.exitFullscreen();
+    }
+};
+
 window.addEventListener('keydown', (event) => {
-    const audio = document.querySelector(`audio[data-letter="${event.keyCode}"]`);
+    if(!event.repeat) {    const audio = document.querySelector(`audio[data-letter="${event.keyCode}"]`);
     const key = document.querySelector(`.piano-key[data-letter="${event.keyCode}"]`)
     if (!audio) return;
     if (!key) return;
     audio.currentTime = 0;
+    console.log(event);
     audio.play();
     key.classList.add('piano-key-active');
+}
 });
 
 window.addEventListener('keyup', (event) => {
@@ -15,11 +44,6 @@ window.addEventListener('keyup', (event) => {
     key.classList.remove('piano-key-active');
 
 });
-
-
-const pianoKeys = document.querySelectorAll('.piano-key');
-const PIANO = document.getElementById('piano');
-
 
 const startSound = (event) => {
     event.target.classList.add('piano-key-active')
@@ -62,3 +86,6 @@ const playaudio = (event) => {
     PIANO.addEventListener('mousedown', startOver, false);
     document.addEventListener('mouseup', stopOver);
     PIANO.addEventListener('mousedown', playaudio);
+    Fullscreen.addEventListener('click', startStopFullscreen);
+    btnNotes.addEventListener('click', notesActive);
+    btnLetters.addEventListener('click', lettersActive);
