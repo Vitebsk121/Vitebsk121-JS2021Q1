882d7c50d4e40076ad5946093de936eb59bf640d
Vitebsk121 <Alexandr.demchenko@list.ru>
2021-04-25 13:35:52 +0300

    feat: add Next picture func

--- photo-filter/script.js
+++ photo-filter/script.js
@@ -1,13 +1,16 @@
 'use strict';
-const Fullscreen = document.querySelector('.fullscreen');
+const fullscreen = document.querySelector('.fullscreen');
 const inputs = document.querySelectorAll('.controls');
 const outputs = document.querySelectorAll('.output')
+const nextPicture = document.querySelector('.btn-next');
+const image = document.getElementById('image');
+let count = 1;
 
 function handleUpdeate() {
     const suffix = this.dataset.sizing || '';
     document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
     outputs.forEach(output => output.name === this.name ? output.value = this.value : output.value);
-}
+};
 
 const startStopFullscreen = () => {
     let elem = document.querySelector('body')
@@ -18,5 +21,33 @@ const startStopFullscreen = () => {
     }
 };
 
-Fullscreen.addEventListener('click', startStopFullscreen);
+function swapPicture() {
+    const date = new Date;
+    const time = date.getHours();
+    let countPrefix = 0;
+    if (count > 9) {
+        countPrefix = '';
+    } else {
+        countPrefix = 0;
+    };
+    if (time >= 6 && time < 12) {
+        image.src = `https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/morning/${countPrefix}${count}.jpg`;
+        count += 1;
+    } else if (time >= 12 && time < 18) {
+        image.src = `https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/day/${countPrefix}${count}.jpg`;
+        count += 1;
+    } else if (time >= 18) {
+        image.src = `https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/evening/${countPrefix}${count}.jpg`;
+        count += 1;
+    } else if (time >= 0 && time < 6) {
+        image.src = `https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/night/${countPrefix}${count}.jpg`;
+        count += 1;
+    };
+    if (count === 21) {
+        count = 1;
+    };
+};
+
+fullscreen.addEventListener('click', startStopFullscreen);
 inputs.forEach(input => input.addEventListener('input', handleUpdeate));
+nextPicture.addEventListener('click', swapPicture);
