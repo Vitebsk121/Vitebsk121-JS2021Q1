9d30e52f9afb6128f3a9edce841cd6fb7d19e545
Vitebsk121 <Alexandr.demchenko@list.ru>
2021-04-25 21:16:06 +0300

    feat: added save picture func

--- photo-filter/script.js
+++ photo-filter/script.js
@@ -6,12 +6,33 @@ const nextPicture = document.querySelector('.btn-next');
 const reset = document.querySelector('.btn-reset');
 const image = document.getElementById('image');
 const loadPicture = document.querySelector('input[type="file"]');
+const canvas = document.querySelector('canvas');
+const savePicture = document.querySelector('.btn-save');
+
 let count = 1;
+let blurValue = '0px';
+let invertValue = '0%';
+let sepiaValue = '0%';
+let saturateValue = '100%';
+let hueValue = '0deg';
+
 
 function handleUpdeate() {
     const suffix = this.dataset.sizing || '';
     document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+    if (this.name === 'blur') {
+        blurValue = this.value * 2 + suffix;
+    } else if (this.name === 'invert') {
+        invertValue = this.value + suffix;
+    } else if (this.name === 'sepia') {
+        sepiaValue = this.value + suffix;
+    } else if (this.name === 'saturate') {
+        saturateValue = this.value + suffix;
+    } else if (this.name === 'hue') {
+        hueValue = this.value + suffix;
+    }
     outputs.forEach(output => output.name === this.name ? output.value = this.value : output.value);
+    drawImage();
 };
 
 const startStopFullscreen = () => {
@@ -48,6 +69,7 @@ function swapPicture() {
     if (count === 21) {
         count = 1;
     };
+    drawImage();
 };
 
 function resetValues() {
@@ -58,6 +80,12 @@ function resetValues() {
     document.documentElement.style.setProperty(`--hue`, '0deg');
     inputs.forEach(item => item.name === 'saturate' ?  item.value = 100 : item.value = 0);
     outputs.forEach(item => item.name === 'saturate' ?  item.value = 100 : item.value = 0);
+    blurValue = '0px';
+    invertValue = '0%';
+    sepiaValue = '0%';
+    saturateValue = '100%';
+    hueValue = '0deg';
+    drawImage();
 };
 
 function upload() {
@@ -67,14 +95,38 @@ function upload() {
         const img = new Image();
         img.src = reader.result;
         image.src = img.src;
+        drawImage();
     }
     reader.readAsDataURL(file);
     loadPicture.value = '';
 };
 
+function drawImage() {
+  const img = new Image();  
+  img.setAttribute('crossOrigin', 'anonymous');
+  img.src = image.src;
+  img.onload = function() {
+    canvas.width = img.width;
+    canvas.height = img.height;
+    const ctx = canvas.getContext("2d");
+    ctx.filter = `blur(${blurValue}) invert(${invertValue}) sepia(${sepiaValue}) saturate(${saturateValue}) hue-rotate(${hueValue})`;
+    ctx.drawImage(img, 0, 0);
+  }; 
+}
+
+function download() {
+    console.log(canvas.toDataURL());
+    var link = document.createElement('a');
+    link.download = 'download.png';
+    link.href = canvas.toDataURL();
+    link.click();
+    link.delete;
+};
 
 inputs.forEach(input => input.addEventListener('input', handleUpdeate));
 fullscreen.addEventListener('click', startStopFullscreen);
 nextPicture.addEventListener('click', swapPicture);
 reset.addEventListener('click', resetValues);
 loadPicture.addEventListener('change', upload);
+savePicture.addEventListener('click', download);
+drawImage();
