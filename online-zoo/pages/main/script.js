3db5683da409b0f39827bf2def38e8e469ea9f48
Vitebsk121 <Alexandr.demchenko@list.ru>
2021-05-04 10:51:11 +0300

    feat: added animals cards to favorite, added scroll to favorite.

--- online-zoo/pages/main/script.js
+++ online-zoo/pages/main/script.js
@@ -1,8 +1,10 @@
 'use strict';
 const ligthTheme = document.querySelectorAll('.light');
 const chbox = document.getElementById('theme');
+const animalsCards = document.querySelectorAll('.animal__card');
 
 swapThemeOnload();
+
 
 function swapTheme() {
     if (chbox.checked) {
@@ -35,5 +37,23 @@ function swapThemeOnload() {
     }
 };
 
+function tratata() {
+    const i = Array.from(animalsCards).indexOf(this);
+    if (animalsCards[i].classList.contains('card__none')) {
+        return;
+    }
+    animalsCards.forEach((item) => item.classList.remove('card__active'));
+    animalsCards[i].classList.add('card__active');
+    const arrOfActive = [animalsCards[i - 1], animalsCards[i], animalsCards[i + 1], animalsCards[i + 2], animalsCards[i + 3]];
+    animalsCards.forEach((item) => {
+        item.classList.remove('hidden-left', 'hidden-right');
+        if (Array.from(animalsCards).indexOf(item) < Array.from(animalsCards).indexOf(arrOfActive[0])) {
+            item.classList.add('hidden-left');
+        } else if (Array.from(animalsCards).indexOf(item) > Array.from(animalsCards).indexOf(arrOfActive[4]) && Array.from(animalsCards).indexOf(arrOfActive[4]) !== -1) {
+            item.classList.add('hidden-right');
+        }
+    })
+};
 
 chbox.addEventListener('click', swapTheme);
+animalsCards.forEach((item) => item.addEventListener('click', tratata));
