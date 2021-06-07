f4bc33be5accbea8579064fb854ac20d96aafff4
Vitebsk121 <Alexandr.demchenko@list.ru>
2021-06-07 14:48:28 +0300

    feat: added car road

--- async-race/src/components/garageField/garageField.ts
+++ async-race/src/components/garageField/garageField.ts
@@ -1,8 +1,11 @@
 import { BaseComponent } from '../base-component';
+import { Car } from '../car/car';
 import './garageField.scss';
 
 export class GarageField extends BaseComponent {
     constructor() {
-        super();
+        super('div', ['garage__field']);
+        const car = new Car();
+        this.element.append(car.element);
     }
 }
