ce720d4600a6c06ec88132cb27a291287b305af5
Vitebsk121 <Alexandr.demchenko@list.ru>
2021-06-08 23:19:27 +0300

    refactor: lint --fix to winnersFooter.ts

--- async-race/src/components/winnersFooter/winnersFooter.ts
+++ async-race/src/components/winnersFooter/winnersFooter.ts
@@ -3,12 +3,12 @@ import { Button } from '../buttons/button';
 import './winnersFooter.scss';
 
 export class WinnersFooter extends BaseComponent {
-    constructor() {
-        super('div', ['winners__footer']);
-        const prevPage = new Button('Prev page', ['winners__button']);
-        this.element.append(prevPage.element);
-    
-        const nextPage = new Button('Next page', ['winners__button']);
-        this.element.append(nextPage.element);
-    }
+  constructor() {
+    super('div', ['winners__footer']);
+    const prevPage = new Button('Prev page', ['winners__button']);
+    this.element.append(prevPage.element);
+
+    const nextPage = new Button('Next page', ['winners__button']);
+    this.element.append(nextPage.element);
+  }
 }
