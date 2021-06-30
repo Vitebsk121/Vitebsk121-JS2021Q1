fafd9284b29ba123494236abca071b4cb91cb305
Vitebsk121 <Alexandr.demchenko@list.ru>
2021-06-30 11:37:41 +0300

    feat: added globalStyle variables

--- english-for-kids/src/components/switcher/switcher.ts
+++ english-for-kids/src/components/switcher/switcher.ts
@@ -1,16 +1,29 @@
+import { switchTheme } from '../../shared/servise';
 import { BaseComponent } from '../baseComponents';
 import './switcher.scss';
 
 export class Switcher extends BaseComponent {
   constructor() {
     super('div', ['onoffswitch']);
-
-    this.element.innerHTML = `
-        <input type="checkbox" name="onoffswitch" class="onoffswitch-checkbox" id="myonoffswitch" tabindex="0" checked>
-        <label class="onoffswitch-label" for="myonoffswitch">
-            <span class="onoffswitch-inner"></span>
-            <span class="onoffswitch-switch"></span>
-        </label>
-        `;
+     const switcherService = () => new Promise<void>((res) => {
+      this.element.innerHTML = `
+      <input type="checkbox" name="onoffswitch" class="onoffswitch-checkbox" id="myonoffswitch" tabindex="0" checked>
+      <label class="onoffswitch-label" for="myonoffswitch">
+          <span class="onoffswitch-inner"></span>
+          <span class="onoffswitch-switch"></span>
+      </label>
+      `;
+      res();
+    });
+    switcherService().then(() => {
+      const switcher = document.getElementById('myonoffswitch');
+      switcher?.addEventListener('input', (e) => {
+        if ((<HTMLInputElement>e.target).checked) {
+          switchTheme('train');
+        } else {
+          switchTheme('play');
+        }
+      });
+    });
   }
 }
--- english-for-kids/src/shared/servise.ts
+++ english-for-kids/src/shared/servise.ts
@@ -12,3 +12,13 @@ export function openCloseNav(): void {
   nav?.classList.toggle('active');
   burger.forEach((line) => line.classList.toggle('active'));
 }
+
+export function switchTheme(themeMode: string): void {
+  const backgroundTheme = document.documentElement.style;
+  console.log(backgroundTheme);
+  if (themeMode === 'train') {
+    backgroundTheme.setProperty('--backgroundStyleMode', 'linear-gradient(158deg, rgba(255,215,0,1) 0%, rgba(0,187,255,1) 23%, rgba(208,77,255,1) 47%, rgba(0,137,255,1) 68%, rgba(0,255,209,1) 93%)');
+  } else if (themeMode === 'play') {
+    backgroundTheme.setProperty('--backgroundStyleMode', 'linear-gradient(158deg, rgba(0,255,209,1) 7%, rgba(172,255,0,1) 19%, rgba(218,255,77,1) 55%, rgba(255,92,92,1) 67%, rgba(255,0,0,1) 100%)');
+  }
+}
