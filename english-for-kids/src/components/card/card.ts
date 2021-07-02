5015f33619b4545e8dc6759b6a5089ae63dba232
Vitebsk121 <Alexandr.demchenko@list.ru>
2021-07-02 23:17:33 +0300

    feat: added train mode play cards, audio was added

--- english-for-kids/src/components/card/card.ts
+++ english-for-kids/src/components/card/card.ts
@@ -42,34 +42,59 @@ export class Card extends BaseComponent {
   }
 
   renderPlayCards(card: CardObj, mode: string): void {
-      this.element.style.background = 'none';
+      this.element.classList.add('game__state');
+      this.element.setAttribute('data-word', `${card.word}`)
     if (mode === 'train') {
       this.card = new BaseComponent('div', ['train-card']);
       this.rotate = new BaseComponent('div', ['rotate']);
-
+      this.flipCard(this.rotate.element);
+      this.unFlipCard(this.rotate.element);
+      
       this.cardFront = new BaseComponent('div', ['train-card__front']);
       this.cardFront.element.style.backgroundImage = `url(${card.image})`;
       this.frontMenu = new BaseComponent('div', ['front__menu'], `${card.word}`);
+      this.playAudio(this.cardFront.element, card);
 
       this.cardBack = new BaseComponent('div', ['train-card__back']);
       this.cardBack.element.style.backgroundImage = `url(${card.image})`;
       this.backMenu = new BaseComponent('div', ['front__menu'], `${card.translation}`);
 
       this.element.append(this.card.element);
-      this.element.append(this.rotate.element);
-      this.card.element.append(this.cardFront.element);
-      this.cardFront.element.append(this.frontMenu.element);
-      this.card.element.append(this.cardBack.element);
+      this.card.element.append(this.cardFront.element, this.cardBack.element);
+      this.cardFront.element.append(this.frontMenu.element, this.rotate.element);
       this.cardBack.element.append(this.backMenu.element);
     } else {
       this.element.classList.add('card__play-mode');
     }
-  }
+  };
 
   addEventListener(categoriesName: string): void {
     this.element.addEventListener('click', () => {
       setNavistStye(categoriesName);
       renderNewMain(categoriesName);
     });
-  }
+  };
+
+  flipCard(button: HTMLElement) {
+    button.addEventListener('click', () => {
+      this.element.classList.add('flipped');
+      button.classList.add('hidden');
+    });
+  };
+
+  unFlipCard(button: HTMLElement) {
+    this.element.addEventListener('mouseleave', () => {
+      this.element.classList.remove('flipped');
+      setTimeout(() => {
+        button.classList.remove('hidden');
+      }, 100);
+    });
+  };
+
+  playAudio(elem: HTMLElement, card: CardObj) {
+    elem.addEventListener('click', (e) => {
+      if ((<HTMLElement>e.target).className === this.rotate.element.className) return;
+      new Audio(card.audioSrc).play();
+    });
+  };
 }
