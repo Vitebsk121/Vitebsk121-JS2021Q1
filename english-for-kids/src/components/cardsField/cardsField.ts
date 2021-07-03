2cf9e50c618e4a600854c57f82fbf989236c1ff8
Vitebsk121 <Alexandr.demchenko@list.ru>
2021-07-03 13:32:46 +0300

    feat: added scoreField, cardsField, gameButton

---
+++ english-for-kids/src/components/cardsField/cardsField.ts
@@ -0,0 +1,35 @@
+import { getCards, getHeadersOfCards } from '../../shared/server';
+import { getMode } from '../../shared/servise';
+import { BaseComponent } from '../baseComponents';
+import { Card } from '../card/card';
+import './cardsField.scss';
+
+export class CardsField extends BaseComponent {
+    private card!: Card;
+
+    constructor(category: string) {
+        super('div', ['cards-field']);
+
+        if (category === 'Main Page') {
+            getHeadersOfCards().then((categories) => {
+              categories.forEach((cat) => {
+                getCards(cat).then((arrOfCards) => {
+                  getMode().then((mode) => {
+                    this.card = new Card(arrOfCards[0], mode, cat);
+                    this.element.append(this.card.element);
+                  });
+                });
+              });
+            });
+          } else {
+            getCards(category).then((arrOfCards) => {
+              getMode().then((mode) => {
+                arrOfCards.forEach((card) => {
+                  this.card = new Card(card, mode);
+                  this.element.append(this.card.element);
+                });
+              });
+            });
+          }
+    }
+}
--- english-for-kids/src/components/main/main.ts
+++ english-for-kids/src/components/main/main.ts
@@ -2,34 +2,22 @@ import { getCards, getHeadersOfCards } from '../../shared/server';
 import { getMode } from '../../shared/servise';
 import { BaseComponent } from '../baseComponents';
 import { Card } from '../card/card';
+import { CardsField } from '../cardsField/cardsField';
+import { ScoreField } from '../scoreField/scoreField';
 import './main.scss';
 
 export class Main extends BaseComponent {
-  private card!: Card;
+  private scoreField: ScoreField;
+  private cardsField: CardsField;
+  private gameButton: BaseComponent;
+
 
   constructor(category: string) {
     super('main', ['app__main']);
 
-    if (category === 'Main Page') {
-      getHeadersOfCards().then((categories) => {
-        categories.forEach((cat) => {
-          getCards(cat).then((arrOfCards) => {
-            getMode().then((mode) => {
-              this.card = new Card(arrOfCards[0], mode, cat);
-              this.element.append(this.card.element);
-            });
-          });
-        });
-      });
-    } else {
-      getCards(category).then((arrOfCards) => {
-        getMode().then((mode) => {
-          arrOfCards.forEach((card) => {
-            this.card = new Card(card, mode);
-            this.element.append(this.card.element);
-          });
-        });
-      });
-    }
+    this.cardsField = new CardsField(category);
+    this.scoreField = new ScoreField();
+    this.gameButton = new BaseComponent('button', ['game-btn'], 'start');
+    this.element.append(this.scoreField.element, this.cardsField.element, this.gameButton.element);
   }
 }
