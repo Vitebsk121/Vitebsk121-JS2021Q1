b13cc848a808c26dbfa47331238a8964e7dedbf5
Vitebsk121 <Alexandr.demchenko@list.ru>
2021-07-07 15:22:43 +0300

    feat: added game

--- english-for-kids/src/components/main/main.ts
+++ english-for-kids/src/components/main/main.ts
@@ -1,3 +1,4 @@
+import { startGame } from '../../shared/gameServise';
 import { BaseComponent } from '../baseComponents';
 import { CardsField } from '../cardsField/cardsField';
 import { ScoreField } from '../scoreField/scoreField';
@@ -10,6 +11,10 @@ export class Main extends BaseComponent {
 
   private gameButton: BaseComponent;
 
+  private repeatButton: BaseComponent;
+
+  private buttonImg: BaseComponent;
+
   constructor(category: string) {
     super('main', ['app__main']);
 
@@ -18,9 +23,24 @@ export class Main extends BaseComponent {
     this.cardsField = new CardsField(category);
     this.scoreField = new ScoreField();
     this.gameButton = new BaseComponent('button', ['game-btn'], 'start');
+    this.repeatButton = new BaseComponent('button', ['repeat-btn', 'hidden'], 'repeat');
+    this.buttonImg = new BaseComponent('img', ['btn-pic']);
+    this.buttonImg.element.setAttribute('src', 'icons/repeat.png');
     if (mode === 'train' || category === 'Main Page') {
       this.gameButton.element.classList.add('hidden');
     }
-    this.element.append(this.scoreField.element, this.cardsField.element, this.gameButton.element);
+    this.repeatButton.element.append(this.buttonImg.element);
+    this.element.append(this.scoreField.element,
+      this.cardsField.element,
+      this.gameButton.element,
+      this.repeatButton.element);
+    this.addEventListener(category);
+  }
+
+  addEventListener(category: string): void {
+    this.gameButton.element.addEventListener('click', () => {
+      startGame(category);
+      this.gameButton.element.classList.add('hidden');
+    });
   }
 }
